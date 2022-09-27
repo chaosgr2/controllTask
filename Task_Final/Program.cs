1300a27276305530a1dcb87ce9ff103bfae44d2c
Vasiliy <vasiliy.tolmachev.vt2@gmail.com>
2022-09-27 13:35:35 +0500

    array_count

--- Task_Final/Program.cs
+++ Task_Final/Program.cs
@@ -14,3 +14,20 @@ Console.WriteLine($"{PrintArray(ArrayStrings)}");
 Console.WriteLine();
 
 int length = 3;
+
+// функция определения количества элемента массива длина которых меньше какой-то заданной длины length
+int CheckArray(string[] array, int length)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= length) result++;
+    }
+    return result;
+}
+
+int numbers = CheckArray(ArrayStrings, length); 
+ 
+
+string[] newArrayStrings = new string[numbers]; 
+NewArray(ArrayStrings, newArrayStrings, length);
